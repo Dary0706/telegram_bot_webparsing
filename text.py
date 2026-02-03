28f5a31c9e51c92980b1a9954e498544e02625e9
Dary <91452787+Dary0706@users.noreply.github.com>
2026-02-03 12:51:18 +0300

    text.py

--- text.py
+++ text.py
@@ -2,6 +2,10 @@ from razdel import sentenize
 
 
 def pipiska(text: str, limit: int = 1024) -> str:
+    text = text.strip()
+    text = text.replace('\n', ' ')
+    text = ' '.join(text.split())
+
     if len(text) <= limit:
         return text
 
@@ -12,11 +16,14 @@ def pipiska(text: str, limit: int = 1024) -> str:
     out_parts = []
     total = 0
     for s in sentences:
-        s_text = text[s.start:s.stop]
+        s_text = s.text
+
         if total + len(s_text) > limit:
             break
         out_parts.append(s_text)
         total += len(s_text)
 
-    return "".join(out_parts).rstrip()
+    return " ".join(out_parts).rstrip()
+
+
 
