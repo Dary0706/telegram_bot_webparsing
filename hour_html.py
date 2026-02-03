31a0752fba677e75558d1f1aa8c94756ba575cc8
Dary <91452787+Dary0706@users.noreply.github.com>
2026-02-03 12:49:02 +0300

    hour_html.py

--- hour_html.py
+++ hour_html.py
@@ -9,10 +9,10 @@ def get_h1_hr(news_url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
 
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, timeout=10)
     soup = BeautifulSoup(r.content, "lxml")
 
-    h1 = soup.h1.string
+    h1 = soup.h1.text[21:][:-17]
     return h1
 
 
@@ -22,15 +22,16 @@ def get_div_hr(news_url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
 
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, timeout=10)
     soup = BeautifulSoup(r.content, "lxml")
 
-    a = soup.find('div', 'PageContentCommonStyling_text__CKOzO')
+    a = soup.find('div', 'article__text')
     if a == None:
         return None
     else:
         div = a.getText()
-        return div
+        c = div.replace("rbc.group", "")
+        return c
 
 
 def get_img_hr(news_url):
@@ -39,14 +40,20 @@ def get_img_hr(news_url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
 
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, timeout=10)
     r.raise_for_status()
     soup = BeautifulSoup(r.content, "lxml")
 
-    pic = soup.find('img', class_='PageArticleContent_imageSrc__54iUt')
+    pic = soup.find('div', {'class': 'article__text'})
     if pic:
-        image = pic.get('src')
-        return image
+        # Внутри этого div ищем тег img
+        img_tag = pic.find('img')
+        if img_tag and 'src' in img_tag.attrs:
+            return img_tag['src']
     else:
         return None
 
+
+# print(get_h1_hr('https://www.rbc.ru/politics/02/02/2026/6980715f9a7947842d201087?from=short_news'))
+# print(get_div_hr('https://www.rbc.ru/education/02/02/2026/69788fc09a7947ce394ffb20?from=short_news'))
+# print(get_img_hr('https://www.rbc.ru/rbcfreenews/698099409a7947249771c0d9?from=short_news'))
